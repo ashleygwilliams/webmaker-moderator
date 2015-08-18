0fecfc6a9d7950c528f2d9506752bff2619248ac
ag_dubs <ashley666ashley@gmail.com>
2015-08-18 16:30:39 -0400

    add author and thumb to model and views

--- app/models/project.js
+++ app/models/project.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   remixed_from: DS.attr('number'),
   version: DS.attr('string'),
+  author: DS.attr(),
   title: DS.attr('string'),
   featured: DS.attr('boolean'),
+  thumbnail: DS.attr()
 });
